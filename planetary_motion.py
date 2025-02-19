242df737513399b71e89318af6043f89fd7033d0
Serjei Ionov <s.ionov1@g.nsu.ru>
2025-02-19 11:31:32 +0700

    done

--- planetary_motion.py
+++ planetary_motion.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
-def planet2D(x_0, y_0, vx_0, vy_0, Mc, m, dt, N):
+def planet2D(x_0, y_0, vx_0, vy_0, Mc, m, dt, N, merge_distance):
     G = 6.67430e-11
     M = Mc
     
@@ -17,21 +17,25 @@ def planet2D(x_0, y_0, vx_0, vy_0, Mc, m, dt, N):
 
     for i in range(1, N):
         r_norm = np.linalg.norm(r[i])
+        if r_norm < merge_distance:
+            r = r[:i+1]
+            break
         a = -G * M * r[i] / r_norm**3
         r[i+1] = 2 * r[i] - r[i-1] + a * dt**2
     
-    return r, t
+    return r, t[:len(r)]
 
 x_0, y_0 = 1.0e15, 0
-vx_0, vy_0 = 0, 2e7
+vx_0, vy_0 = 0, 1e7
 Mc, m = 9.945e39, 1.989e39
-dt, N = 60*60*24, 1700
+dt, N = 60*60*24, 1000
+merge_distance = 3.8e13
 
-r, t = planet2D(x_0, y_0, vx_0, vy_0, Mc, m, dt, N)
+r, t = planet2D(x_0, y_0, vx_0, vy_0, Mc, m, dt, N, merge_distance)
 
 fig, ax = plt.subplots(figsize=(8, 8))
-ax.set_xlim(np.min(r[:, 0]) * 1.1, np.max(r[:, 0]) * 1.1)
-ax.set_ylim(np.min(r[:, 1]) * 1.1, np.max(r[:, 1]) * 1.1)
+ax.set_xlim(-2e15, 2e15)
+ax.set_ylim(-2e15, 2e15)
 ax.set_xlabel('X (m)')
 ax.set_ylabel('Y (m)')
 ax.grid()
@@ -51,5 +55,5 @@ def update(frame):
     trail.set_data(r[:frame+1, 0], r[:frame+1, 1])
     return planet, trail
 
-ani = animation.FuncAnimation(fig, update, frames=N, init_func=init, blit=True, interval=1)
+ani = animation.FuncAnimation(fig, update, frames=len(r), init_func=init, blit=True, interval=1)
 plt.show()
